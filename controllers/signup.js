1ed6c4e2043ca5d4fd5d48cf7b7803fd3b14ae12
chelsey hill <chelsey.c.hill@gmail.com>
2021-03-23 20:39:03 -0400

    signup routing complete

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,6 +1,7 @@
 var SignupController = {
     Index: function (req, res) {
       res.render("signup/index");
-    }},
-    module,exports = SignupController;
+    }
+  };
+    module.exports = SignupController;
   
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,10 @@
-/*var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 var SignupController = require('../controllers/signup')
 
 router.get('/', SignupController.Index);
-router.post('/', SignupController.Create);
-
-module.exports = router;*/
+router.post('/signup/index', function(req, res){
+    Controller.Create
+  });
+module.exports = router;
